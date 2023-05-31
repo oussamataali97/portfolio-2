41075ed4644d5a38f7054fffdbf0e301c7df33a4
Oussama Taali <oussamataali1997@gmail.com>
2023-05-31 16:12:30 +0100

    add project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,6 +11,9 @@ import spot from './../assets/spot.png'
 import multi from './../assets/multi.jpg'
 import hurley from './../assets/hurley.png'
 import prod10 from './../assets/youtube.PNG'
+import vete from './../assets/vete.jpg'
+import startup from './../assets/startup.jpg'
+
 import { motion } from "framer-motion"
 
 
@@ -26,7 +29,23 @@ const Porfolio = ({theme}) => {
     }}>
                 <p className=' font-[700] border-b-4 w-fit border-b-mainblue mb-16  '>Portfolio</p>
     <div className="cards grid md:grid-cols-3 gap-5">
-    <a href="https://multi-step-form-react-redux-toolkit.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg" >
+           <a href="https://startup-agency-nextjs-five.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg" >
+            <img src={startup} alt="" className='w-full max-h-[150px] object-cover shadow-md' />
+            <div className="py-5">
+            <p className=' ml-5 font-[700] mt-5 mb-1   '>Startup Agency</p>
+            <p className=' ml-5 font-[400]  '>Landing page (Tailwind,Next js, AOS)</p>
+
+            </div>
+        </div></a>
+    <a href="https://sunny-sprinkles-430b07.netlify.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg" >
+            <img src={vete} alt="" className='w-full object-cover shadow-md' />
+            <div className="py-5">
+            <p className=' ml-5 font-[700] mt-5 mb-1   '>Veterinary Landing page</p>
+            <p className=' ml-5 font-[400]  '>Landing page (Tailwind, React js)</p>
+
+            </div>
+        </div></a>
+    <a href="https://multi-step-form-react-redux-toolkit.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg" >
             <img src={multi} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Multi Step Form</p>
@@ -35,7 +54,7 @@ const Porfolio = ({theme}) => {
             </div>
         </div></a>
 
-    <a href="https://store-omega-smoky.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg" >
+    <a href="https://store-omega-smoky.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg" >
             <img src={hurley} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>E-commerce Store</p>
@@ -44,7 +63,7 @@ const Porfolio = ({theme}) => {
             </div>
         </div></a>
 
-    <a href="https://spotify-clone-two-sandy.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg" >
+    <a href="https://spotify-clone-two-sandy.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg" >
             <img src={spot} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Spotify Clone</p>
@@ -52,7 +71,7 @@ const Porfolio = ({theme}) => {
 
             </div>
         </div></a>
-         <a href="https://gthconsult-website.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg" >
+         <a href="https://gthconsult-website.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg" >
             <img src={prod1} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Gth Website</p>
@@ -60,63 +79,63 @@ const Porfolio = ({theme}) => {
 
             </div>
         </div></a>
-        <a href="https://msi-app-player.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm  md:h-64 hover:cursor-pointer hover:shadow-lg">
+        <a href="https://msi-app-player.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm  md:h-auto hover:cursor-pointer hover:shadow-lg">
             <img src={prod2} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Msi Landing Page</p>
             <p className=' ml-5 font-[400]  '>Clone landing page MSI app player</p>
                 </div>
         </div></a>
-        <a href="https://fylo-landing-page-with-two-column-layout-ssyk.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg">
+        <a href="https://fylo-landing-page-with-two-column-layout-ssyk.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg">
             <img src={prod8} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Fyloo Landing page</p>
             <p className=' ml-5 font-[400]  '>Landing page using react + Tailwindcss</p>
                 </div>
         </div></a>
-        <a href="https://deliveroo-website-reactjs.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg">
+        <a href="https://deliveroo-website-reactjs.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg">
             <img src={prod6} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Deliveroo Clone Website</p>
             <p className=' ml-5 font-[400]  '>Clone landing page Deliveroo</p>
                 </div>
         </div></a>
-        <a href="https://youtube-clone-sooty-ten.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg">
+        <a href="https://youtube-clone-sooty-ten.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg">
             <img src={prod10} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Youtube Clone Website</p>
             <p className=' ml-5 font-[400]  '>Clone landing page Youtube using Tailwindcss</p>
                 </div>
         </div></a>
-        <a href="https://github-finder-react-app-three.vercel.app//" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg">
+        <a href="https://github-finder-react-app-three.vercel.app//" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg">
             <img src={prod7} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Github Finder</p>
             <p className=' ml-5 font-[400]  '>Github Finder using react + Tailwindcss</p>
                 </div>
         </div></a>
-        <a href="https://hoobank-three-olive.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg">
+        <a href="https://hoobank-three-olive.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg">
             <img src={prod3} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Hoobank Landing page</p>
             <p className=' ml-5 font-[400]  '>Landing page with Tailwindcss and react js</p>
                 </div>
         </div></a>
-        <a href="https://home-market-place-gamma.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg">
+        <a href="https://home-market-place-gamma.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg">
             <img src={prod4} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>Home Market</p>
             <p className=' ml-5 font-[400]  '>Estate App Wich you can Sell or rent houses</p>
                 </div>
         </div></a>
-        <a href="https://easybank-landing-page-eight-indol.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg">
+        <a href="https://easybank-landing-page-eight-indol.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg">
             <img src={prod5} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>EasyBank Landing Page</p>
             <p className=' ml-5 font-[400]  '>A Landing page created using Tailwind css and react</p>
                 </div>
         </div></a>
-        <a href="https://cocktail-seven.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-64 hover:cursor-pointer hover:shadow-lg">
+        <a href="https://cocktail-seven.vercel.app/" target='_blank' rel="noreferrer"><div className="hover:scale-105 ease-linear duration-200 card shadow-sm md:h-auto hover:cursor-pointer hover:shadow-lg">
             <img src={prod9} alt="" className='w-full object-cover shadow-md' />
             <div className="py-5">
             <p className=' ml-5 font-[700] mt-5 mb-1   '>CocktailDB API</p>
